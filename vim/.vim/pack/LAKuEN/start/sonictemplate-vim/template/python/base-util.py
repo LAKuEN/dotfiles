2112ecf123ce6bf1c304ac4c47dd8309e2bd11cb
ysk_brave <y.s.k.brave@gmail.com>
2017-05-22 00:12:12 +0900

    sonictemplate-vimと、ユーティリティ用のテンプレートファイルを追加

---
+++ vim/.vim/pack/LAKuEN/start/sonictemplate-vim/template/python/base-util.py
@@ -0,0 +1,23 @@
+# coding: UTF-8
+'''Summary
+
+Usage:
+    python {{_name_}}
+'''
+import codecs
+import csv
+import os
+from pprint import pprint
+import sys
+
+
+def main():
+    {{_cursor_}}
+    return
+
+
+if __name__ == "__main__" and len(sys.argv) == 2:
+    main(*sys.argv[1:])
+else:
+    print("{0}\n{1}\n{0}".format("="*100, __doc__))
+
