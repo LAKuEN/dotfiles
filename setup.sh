5ead04a7952ce0bb5de48e8f3cf139fe58ceba25
LAKuEN <y.s.k.brave@gmail.com>
2018-10-01 10:42:02 +0900

    :fix setup.shのln先が実態にあってなかったので修正

--- setup.sh
+++ setup.sh
@@ -3,9 +3,9 @@ echo 'export XDG_CONFIG_HOME="$HOME/.config"' >> $HOME/.bashrc && source $HOME/.
 mkdir -p $XDG_CONFIG_HOME
 
 # Vim
-ln -nfs $HOME/git/dotfiles/vim/.vim $HOME/.vim
-ln -nfs $HOME/git/dotfiles/vim/.vimrc $HOME/.vimrc
+ln -nfs $HOME/repo/dotfiles/vim/.vim $HOME/.vim
+ln -nfs $HOME/repo/dotfiles/vim/.vimrc $HOME/.vimrc
 
 # NeoVim
-ln -nfs $HOME/git/dotfiles/vim/.vim $XDG_CONFIG_HOME/nvim
-ln -nfs $HOME/git/dotfiles/vim/.vimrc $XDG_CONFIG_HOME/nvim/init.vim
+ln -nfs $HOME/repo/dotfiles/vim/.vim $XDG_CONFIG_HOME/nvim
+ln -nfs $HOME/repo/dotfiles/vim/.vimrc $XDG_CONFIG_HOME/nvim/init.vim
