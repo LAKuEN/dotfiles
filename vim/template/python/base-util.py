fe4448e0c02db492cc39c149be24b2e6cc98b8ea
LAKuEN <y.s.k.brave@gmail.com>
2019-01-23 11:07:45 +0900

    :feat dein.toml更新＆sonictemplate-vimのテンプレートを修正

--- vim/template/python/base-util.py
+++ vim/template/python/base-util.py
@@ -1,26 +1,17 @@
-# coding: UTF-8
-"""Summary
+# -*- coding: utf-8 -*-
+"""{{module description}}
 """
+
+
 import argparse
-import codecs
-import csv
-import os
-import sys
 
 
 def main():
     {{_cursor_}}
-    return
+    pass
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument("arg1", help="",
-                        type=int)
-    parser.add_argument("--arg2", help="",
-                        type=int, default=1)
-    parser.add_argument("--bool_arg", help="",
-                        action="store_true")
-    args = parser.parse_args()
 
     main()
