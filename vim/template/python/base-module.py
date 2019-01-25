afca6dc0e3c9c5b9977449a76aa361f9972273fc
LAKuEN <y.s.k.brave@gmail.com>
2019-01-26 02:30:59 +0900

    :feat sonictemplateの独自テンプレートを追加

---
+++ vim/template/python/base-module.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+"""module description
+"""
+def func():
+    {{_cursor_}}pass
---
+++ vim/template/python/base-test.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import unittest
+
+
+class Test{{_input_:funcname}}Methods(unittest.TestCase):
+    def test_case(self):
+        {{_cursor_}}pass
+
+
+if __name__ == '__main__':
+    unittest.main()
