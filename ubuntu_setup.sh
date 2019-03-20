b765b0a1201a14d0ba67e4bc0d4f7d89160c229c
y.s.k.brave@gmail.com <y.s.k.brave@gmail.com>
2019-03-20 18:16:15 +0900

    :fix

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -18,7 +18,7 @@ sudo apt update
 sudo apt install -y albert
 
 sudo apt update
-sudo apt install -y asunder chrome-gnome-shell clamav-base clamav-freshclam davfs2 dkms exfat-fuse exfat-utils fcitx-mozc ffmpeg fonts-ricty-diminished gimp git gnome-shell gnome-sushi gnome-tweak-tool gnome-tweaks golang gparted gucharmahp imagemagick jq neovim nodejs octave python3 python3-pip python3-venv solaar tig timeshift tlp tlp-rdw tmux translate-shell tree vim virtualbox zsh
+sudo apt install -y asunder chrome-gnome-shell clamav-base clamav-freshclam davfs2 dkms exfat-fuse exfat-utils fcitx-mozc ffmpeg fonts-ricty-diminished gimp git gnome-shell gnome-sushi gnome-tweak-tool gnome-tweaks gparted imagemagick jq neovim nodejs octave python3 python3-pip python3-venv solaar tig timeshift tlp tlp-rdw tmux translate-shell tree vim virtualbox zsh
 # ネットワーク接続系
 # VPN
 sudo apt install network-manager-openconnect-gnome
@@ -30,14 +30,12 @@ chsh -s $(which zsh)
 im-config -n fcitx
 sudo -H pip3 install pynvim
 
-mkdir $HOME/repo && cd $HOME/repo
+sudo mkdir $HOME/repo
 cd $HOME/repo
-git clone https://github.com/LAKuEN/dotfiles.git
-shopt -s expand_aliases
 
 # golang
 curl -L -O https://dl.google.com/go/go1.12.1.linux-amd64.tar.gz
-tar -C /usr/local -xzf go1.12.1.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.12.1.linux-amd64.tar.gz
 
 # 絵文字対応のフォント突っ込む
 curl -O https://github.com/miiton/Cica/releases/download/v4.1.1/Cica-v4.1.1.zip
@@ -49,27 +47,24 @@ UUID=$(gsettings get org.gnome.Terminal.ProfilesList default | tr -d \')
 gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:${UUID}/ font "Cica Bold 11"
 rm Cica-v4.1.1.zip Cica-*.ttf
 
-source setup.sh
-
 # set keybinding in mozc with gui
 # * Alt: IME ON/OFF
 # set keybinding in setting
 # * Ctrl+Alt+h,l: move current window
 
-if [ $(echo $SHELL) = "/usr/bin/bash" ]; then
-    sudo ln -nfs $HOME/repo/dotfiles/.bashrc $HOME/.bashrc
-    shopt -s expand_aliases
-    source $HOME/.bashrc
-fi
+# echo "# bash"
+# sudo ln -nfs $HOME/repo/dotfiles/.bashrc $HOME/.bashrc
+# shopt -s expand_aliases
+# source $HOME/.bashrc
 
-if [ $(echo $SHELL) = "/usr/bin/zsh" ]; then
-    sudo ln -nfs $HOME/repo/dotfiles/.zshrc $HOME/.zshrc
-    sudo ln -nfs $HOME/repo/dotfiles/.zshenv $HOME/.zshenv
-    sudo ln -nfs $HOME/repo/dotfiles/.zpreztorc $HOME/.zpreztorc
-    source $HOME/.zshrc
-    source $HOME/.zshenv
-    source $HOME/.zpreztorc
-fi
+echo "# zsh"
+sudo ln -nfs $HOME/repo/dotfiles/.zshrc $HOME/.zshrc
+sudo ln -nfs $HOME/repo/dotfiles/.zshenv $HOME/.zshenv
+sudo ln -nfs $HOME/repo/dotfiles/.zpreztorc $HOME/.zpreztorc
+shopt -s expand_aliases
+source $HOME/.zshrc
+source $HOME/.zshenv
+source $HOME/.zpreztorc
 
 sudo ln -nfs $HOME/repo/dotfiles/.tmux.conf $HOME/.tmux.conf
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
@@ -79,8 +74,7 @@ tmux source ~/.tmux.conf
 git config --global core.editor nvim
 git config --global core.quotepath false
 
-mkdir -p $XDG_CONFIG_HOME
-mkdir -p $XDG_CONFIG_HOME/nvim
+sudo mkdir -p $XDG_CONFIG_HOME/nvim
 
 # Vim
 sudo ln -nfs $HOME/repo/dotfiles/vim/.vim $HOME/.vim
